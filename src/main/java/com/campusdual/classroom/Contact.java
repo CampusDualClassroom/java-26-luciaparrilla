62df422a7ff0279d052769e20a1d83c882ab398e
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-11-04 15:59:27 +0100

    Terminado 26

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,95 @@
 package com.campusdual.classroom;
 
-public class Contact{
+import java.text.Normalizer;
 
+public class Contact implements ICallActions{
+    private String name;
+    private String surnames;
+    private String phone;
+
+
+    public Contact(String name, String surnames, String phone) {
+        this.name = name;
+        this.surnames = surnames;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return this.surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int numberPhone) {
+        this.phone = phone;
+    }
+
+    public String getCode() {
+
+        String lowerName = this.name.toLowerCase();
+        String lowerSurnames = this.surnames.toLowerCase();
+
+        String diacriticsName = Normalizer.normalize(lowerName, Normalizer.Form.NFD);
+        String diacriticsSurnames = Normalizer.normalize(lowerSurnames, Normalizer.Form.NFD);
+
+        String perfectName = diacriticsName.replaceAll("[^\\p{ASCII}]", "");
+        String perfectSurnames = diacriticsSurnames.replaceAll("[^\\p{ASCII}]", "");
+
+        String[] nameParts = perfectName.split(" ");
+        String[] surnamesPart = perfectSurnames.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(nameParts[0].charAt(0));
+
+        if(perfectSurnames.contains(" ")) {
+
+            sb.append(surnamesPart[0].charAt(0));
+            for (int i = 1; i < surnamesPart.length; i++) {
+                sb.append(surnamesPart[i]);
+            }
+        }
+        else {
+            sb.append(perfectSurnames);
+        }
+
+        return sb.toString();
+    }
+
+
+
+    @Override
+    public void callMyNumber() {
+        System.out.println("You are calling yourself (" + this.name + this.surnames + ", " + this.phone);
+
+    }
+
+    @Override
+    public void callOtherNumber(String number) {
+        System.out.println("You are calling " + this.name + " " + this.surnames + " - " + number);
+
+    }
+
+    @Override
+    public void showContactDetails() {
+        System.out.println("Contact details: " + this.name + " " + this.surnames
+                + " " + this.phone + " " + this.getCode());
+
+    }
 }
+
+
--- src/main/java/com/campusdual/classroom/ICallActions.java
+++ src/main/java/com/campusdual/classroom/ICallActions.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public interface ICallActions {
+public interface ICallActions  {
     void callMyNumber();
     void callOtherNumber(String number);
     void showContactDetails();
